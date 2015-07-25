3ae77b38d1bf20ad0793bf4fbdc34c11aa062545
Mohamed Rashad <thegreatrashad@yahoo.com>
2015-07-25 12:18:23 +0200

    Update RoboType.java

--- Source/java/com/pidevelopers/robotype/RoboType.java
+++ Source/java/com/pidevelopers/robotype/RoboType.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2015 Mohammed Rashad
- * Copyright (C) 2015 Sahid S Almas
+ * Copyright (C) 2015 Pi Developers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * @author mohammed rashad , sahid almas
  */
 package com.pidevelopers.robotype;
 
